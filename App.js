db149f20cb1390110c81dd4fc2bbf13f589d6792
ggtmtmgg <xxx.tubasa0226@gmail.com>
2018-07-21 15:52:39 +0900

    WIP firebaseのTwitter OAuth実装途中

--- App.js
+++ App.js
@@ -2,6 +2,29 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { firebase } from './firebase';
 
+
+// TODO: 適切な場所に移動する
+const provider = new firebase.auth.TwitterAuthProvider();
+// firebase.auth().languageCode = 'ja';
+// firebase.auth().languageCode = 'ja';
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+  // You can use these server side with your app's credentials to access the Twitter API.
+  const token = result.credential.accessToken;
+  const secret = result.credential.secret;
+  // The signed-in user info.
+  const user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  const errorCode = error.code;
+  const errorMessage = error.message;
+  // The email of the user's account used.
+  const email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  const credential = error.credential;
+});
+
 export default class App extends React.Component {
   render() {
     return (
