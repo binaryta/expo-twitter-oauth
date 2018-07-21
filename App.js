48a08d52a41df53f79b73d398e79932cc3f771f6
ggtmtmgg <xxx.tubasa0226@gmail.com>
2018-07-21 19:36:59 +0900

    WIP Twitter OAuth実装途中

--- App.js
+++ App.js
@@ -1,51 +1,91 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { firebase, login } from './firebase';
-import * as fb from 'firebase';
-import { NativeModules } from 'react-native'
+import {StyleSheet, Text, View, Button} from 'react-native';
+import {config} from './config';
+import twitter, {TWLoginButton, decodeHTMLEntities, getRelativeTime} from 'react-native-simple-twitter'
 
-// const provider = firebase.auth.FacebookAuthProvider;
-// const providerTwitter =firebase.auth.TwitterAuthProvider;
 const Constants = {
   TWITTER_COMSUMER_KEY: 'uOiSkazdnmcQYpeI0r144286A',
   TWITTER_CONSUMER_SECRET: 'KpJ2CkeYQcbl7vDAyKWCFxvg6J95RURl7FLsYmM8PqZceTIChC',
 };
 
 export default class App extends React.Component {
-  handleSubmit = () => {
-    const RNTwitterSignIn = NativeModules.RNTwitterSignIn;
-    console.log('------------')
-    console.log(NativeModules)
-    console.log(RNTwitterSignIn)
-    console.log('------------')
-    RNTwitterSignIn.init(Constants.TWITTER_COMSUMER_KEY, Constants.TWITTER_CONSUMER_SECRET);
-    RNTwitterSignIn.logIn()
-      .then((loginData)=>{
 
-        const { authToken, authTokenSecret } = loginData;
-        if (authToken && authTokenSecret) {
-          // we are loged successfull
+  async componentWillMount() {
+    if (this.props.user.token) {
+      twitter.setAccessToken(this.props.user.token, this.props.user.token_secret)
 
-          const credential = providerTwitter.credential(authToken, authTokenSecret);
-          return auth.signInWithCredential(credential);
-        }
-      }).then(credData => {
-        //..
-      }).catch((error)=>{
+      try {
+        const user = await twitter.get("account/verify_credentials.json", {
+          include_entities: false,
+          skip_status: true,
+          include_email: true
+        })
+        this.props.dispatch({type: "USER_SET", user: user})
 
-        props.dispatch(ACTIONS.UPDATE_SHOW_LOGGIN_CONTENT(true))
-      });
+        this.props.dispatch(NavigationActions.reset({
+          index: 0,
+          actions: [
+            NavigationActions.navigate({routeName: 'Home'})
+          ]
+        }))
+      } catch (err) {
+        console.log(err)
+      }
+    }
   }
+
+  onGetAccessToken = ({ oauth_token, oauth_token_secret }) => {
+    this.props.dispatch({ type: "TOKEN_SET", token: oauth_token, token_secret: oauth_token_secret })
+  }
+
+  onSuccess = (user) => {
+    this.props.dispatch({ type: "USER_SET", user: user })
+
+    Alert.alert(
+      "Success",
+      "ログインできました",
+      [
+        {
+          text: 'Go HomeScreen',
+          onPress: () => {
+            this.props.dispatch(NavigationActions.reset({
+              index: 0,
+              actions: [
+                NavigationActions.navigate({ routeName: 'Home' })
+              ]
+            }))
+          }
+        }
+      ]
+    )
+  }
+
+
+
+onPress = e => console.log("button pressed")
+  onClose = e => console.log("press close button")
+  onError = err => console.log(err)
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Button onPress={this.handleSubmit} title='Twitter Log in'/>
+        <View style={styles.title}>
+          <Text style={styles.titleText}>Login</Text>
+        </View>
+        <TWLoginButton
+          style={{ width: "100%", height: 60 }}
+          type="TouchableOpacity"
+          onPress={this.onPress}
+          onGetAccessToken={this.onGetAccessToken}
+          onSuccess={this.onSuccess}
+          onClose={this.onClose}
+          onError={this.onError}>
+          <Text style={{ textAlign: "center", color: "#fff" }}>
+            Twitterでログインする
+          </Text>
+        </TWLoginButton>
       </View>
-    );
+    )
   }
 }
 
